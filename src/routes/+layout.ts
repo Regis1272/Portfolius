5fca0f10a2ed214ff29bee3a6f7e2771db0cc0f5
Regis1272 <21206554+Regis1272@users.noreply.github.com>
2023-10-02 15:12:26 -0700

    nit

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,7 @@
+// svelte will crawl the pages and build them at run-time,
+// so the static files will be served instead
+export const prerender = true
+
 export async function load({ url }) {
     return {
         url: url.pathname

90ecc8ca84813a761dc24933fb29eb91a1b4c24e
isCordial <21206554+isCordial@users.noreply.github.com>
2023-10-20 14:56:24 -0700

    wrapped in <header> tag

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -3,28 +3,30 @@
     import Toggle from './toggle.svelte'
 </script>
 
-<nav>
-    <a href="/">
-        <b>{config.title}</b>
-    </a>
+<header>
+    <nav>
+        <a href="/">
+            <b>{config.title}</b>
+        </a>
 
-    <ul class="links">
-        <li>
-            <a href="/">Blog</a>
-        </li>
-        <li>
-            <a href="/about">About</a>
-        </li>
-        <li>
-            <a href="/contact">Contact</a>
-        </li>
-        <li>
-            <a href="/rss.xml" target="_blank">RSS</a>
-        </li>
-    </ul>
+        <ul class="links">
+            <li>
+                <a href="/">Blog</a>
+            </li>
+            <li>
+                <a href="/about">About</a>
+            </li>
+            <li>
+                <a href="/contact">Contact</a>
+            </li>
+            <li>
+                <a href="/rss.xml" target="_blank">RSS</a>
+            </li>
+        </ul>
 
-    <Toggle />
-</nav>
+        <Toggle />
+    </nav>
+</header>
 
 <style>
     nav {

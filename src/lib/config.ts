68e59193111ef8361a88114b1febfa25b3fac952
isCordial <21206554+isCordial@users.noreply.github.com>
2023-10-20 10:37:35 -0700

    branding changes

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment'
 
-export const title = 'Regis the Dev'
+export const title = 'kordial.dev'
 export const description = 'Regis, aspiring software engineer'
 export const url = dev ? 'http://localhost:5173' : 'http://regis.xyz'
